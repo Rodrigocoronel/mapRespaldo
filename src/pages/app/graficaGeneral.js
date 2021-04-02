b0a9c352f3780a539c57566416585e6df00506d8
Rico <56974483+desarrollowenatives@users.noreply.github.com>
2021-04-02 15:14:06 -0700

    Update graficaGeneral.js

--- src/pages/app/graficaGeneral.js
+++ src/pages/app/graficaGeneral.js
@@ -29,23 +29,25 @@ export default class GraficaGeneral extends Component{
 		console.log(data)
 		return(
 			<div>
-	            <div className="small mb-4 card-subtitle"></div>
-	            <div align="center" style={{width: '100%', height: '280px' , paddingTop : '40px'}}>
-	            	<div align="center" style={{paddingBottom : '20px'}}>
-	            		<label>Promedio General Distrito {this.props.name}</label>
-	                </div>
-	                    <BarChart width={550} height={250} data={data}>
-	                      <CartesianGrid strokeDasharray="3 3" />
-	                      <XAxis dataKey="label" />
-	                      <YAxis />                                  
-							  <Tooltip/>
-	                      <Legend title={'General'}/>                            
-	                      <Bar dataKey="pan" fill='blue' />
-	                      <Bar dataKey='pri' fill='red' />
-	                      <Bar dataKey='morena' fill='brown' />
-	                      <Bar dataKey='otros' fill='gray' />
-	                     </BarChart>
-	            </div>
+			{
+	        //     <div className="small mb-4 card-subtitle"></div>
+	        //     <div align="center" style={{width: '100%', height: '280px' , paddingTop : '40px'}}>
+	        //     	<div align="center" style={{paddingBottom : '20px'}}>
+	        //     		<label>Promedio General Distrito {this.props.name}</label>
+	        //         </div>
+	        //             <BarChart width={550} height={250} data={data}>
+	        //               <CartesianGrid strokeDasharray="3 3" />
+	        //               <XAxis dataKey="label" />
+	        //               <YAxis />                                  
+							  // <Tooltip/>
+	        //               <Legend title={'General'}/>                            
+	        //               <Bar dataKey="pan" fill='blue' />
+	        //               <Bar dataKey='pri' fill='red' />
+	        //               <Bar dataKey='morena' fill='brown' />
+	        //               <Bar dataKey='otros' fill='gray' />
+	        //              </BarChart>
+	        //     </div>
+	        }
             </div>
 		)
 	}
